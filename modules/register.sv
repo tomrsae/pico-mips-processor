6f8f3323c4612d29a5bcccb8758ac31e77c8afed
tomrsae <tommy.saetre@hotmail.com>
2024-03-25 18:02:38 +0000

    Fixed register bug

--- modules/register.sv
+++ modules/register.sv
@@ -1,7 +1,7 @@
 module register #(parameter N = 8) (
     input logic [N-1:0] D,
     input logic clk, n_reset,
-    output logic [N-1] Q
+    output logic [N-1:0] Q
 );
 
 always_ff @(posedge clk, negedge n_reset) begin
